02ff6e2b453931d54114ff7b047db320107be3c3
Dimitrios Paschalis <paschouale@hotmail.com>
2024-10-17 15:09:40 +0300

    Added enumerations

--- src/com/travelcompany/eshop/TravelCompanyEshop.java
+++ src/com/travelcompany/eshop/TravelCompanyEshop.java
@@ -1,4 +1,9 @@
 package com.travelcompany.eshop;
 
+import com.travelcompany.eshop.enumeration.Category;
+
 public class TravelCompanyEshop {
+    public static void main(String[] args) {
+
+    }
 }
--- src/com/travelcompany/eshop/domain/Customer.java
+++ src/com/travelcompany/eshop/domain/Customer.java
@@ -1,10 +1,15 @@
 package com.travelcompany.eshop.domain;
 
+import com.travelcompany.eshop.enumeration.Category;
+import com.travelcompany.eshop.enumeration.Nationality;
+
 public class Customer {
     private Long id;
     private String name;
     private String email;
     private String address;
-    private String nationality;
-    private String category;
+    private Nationality nationality;
+    private Category category;
+
+
 }
